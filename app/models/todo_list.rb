ba0e638a72421437c3629daf57487260f2e494e9
Leandran <leandranm@outlook.com>
2021-10-16 17:33:51 +0200

    Created todo_item model with association/reference

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,4 @@
 class TodoList < ApplicationRecord
+    #Association
+    has_many :todo_items
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
   resources :todo_lists
+  root to: "todo_lists#index"  #Route for the Homepage
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
